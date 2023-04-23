e963e84a1e93d87688f1f0acdd6a9dd17dbbcc87
youn0212 <101792911+youn0212@users.noreply.github.com>
2023-04-23 17:16:34 +0200

    f

---
+++ Assets/Scripts/Multiplayer/SpawnPoints1.cs
@@ -0,0 +1,22 @@
+//using UnityEngine;
+//using UnityEngine.Networking;
+//using Unity.Netcode;
+
+
+//public class NetworkPrefabSelector : MonoBehaviour
+//{
+  //  public GameObject serverPrefab;
+    //public GameObject clientPrefab;
+
+    //private void Start()
+    //{
+      //  if (NetworkServer.active)
+        //{
+         //   Instantiate(serverPrefab);
+        //}
+        //else
+        //{
+         //   Instantiate(clientPrefab);
+        //}
+    //}
+//}
--- Assets/Scripts/Player/CameraScript.cs
+++ Assets/Scripts/Player/CameraScript.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Unity.Netcode;
 
-
-public class CameraScript : MonoBehaviour
-
-
+public class CameraScript : NetworkBehaviour
 {
     [Header("Camera Rotation")]
     public float mouseSensitivity = 5.0f;
@@ -15,17 +13,27 @@ public class CameraScript : MonoBehaviour
     public Transform playerbody;
     float xRotation = 0;
 
-    
-
-    void Start()
+    private void Start()
     {
+        if (!IsLocalPlayer())
+        {
+            // Disable the camera for non-local players
+            gameObject.SetActive(false);
+            return;
+        }
+
         Cursor.lockState = CursorLockMode.Locked;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!IsLocalPlayer())
+        {
+            // Don't update the camera for non-local players
+            return;
+        }
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -34,9 +42,18 @@ public class CameraScript : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 90, 0);
         playerbody.Rotate(Vector3.up * mouseX);
-
-        
     }
 
-   
+   public new bool IsLocalPlayer()
+    {
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening)
+        {
+            if (NetworkManager.Singleton.LocalClientId == base.OwnerClientId)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
